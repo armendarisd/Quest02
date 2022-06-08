94f3d2962f16e943608524a33c8ec896d2df473b
daniel barrera <barrera_d@qwasar.io>
2022-06-08 01:21:45 +0000

    unit is done

---
+++ ex03/my_first_param_function.py
@@ -0,0 +1,11 @@
+# function will print("detonation in... "+str(seconds_left)+" secondes.")
+def detonation_in(timer):
+    seconds_left = timer
+    print("detonation in... "+str(seconds_left)+" secondes.")
+
+
+timer = 10
+
+while (timer > 0):
+  detonation_in(timer)
+  timer -= 1
